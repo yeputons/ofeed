745c566d12aa2c0d637ab56321be5a66156eef43
Egor Suvorov <egor.suvorov@gmail.com>
2015-12-01 11:22:20 +0300

    MainActivity: make it show user's full name

--- src/net/yeputons/ofeed/MainActivity.java
+++ src/net/yeputons/ofeed/MainActivity.java
@@ -7,7 +7,8 @@ import android.widget.Toast;
 import com.vk.sdk.VKAccessToken;
 import com.vk.sdk.VKCallback;
 import com.vk.sdk.VKSdk;
-import com.vk.sdk.api.VKError;
+import com.vk.sdk.api.*;
+import com.vk.sdk.api.model.*;
 
 public class MainActivity extends Activity implements VKCallback<VKAccessToken> {
     @Override
@@ -26,8 +27,25 @@ public class MainActivity extends Activity implements VKCallback<VKAccessToken> 
     }
 
     @Override
-    public void onResult(VKAccessToken res) {
-        Toast.makeText(this, "OnResult: " + res.userId, Toast.LENGTH_LONG).show();
+    public void onResult(final VKAccessToken res) {
+        Toast.makeText(this, "OnResult: " + res.userId, Toast.LENGTH_SHORT).show();
+        VKApi.users().get().executeWithListener(new VKRequest.VKRequestListener() {
+            @Override
+            public void onComplete(VKResponse response) {
+                VKApiUser user = (VKApiUser) ((VKList) response.parsedModel).get(0);
+                Toast.makeText(MainActivity.this, user.first_name + " " + user.last_name, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(VKError error) {
+                Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onProgress(VKRequest.VKProgressType progressType, long bytesLoaded, long bytesTotal) {
+                Toast.makeText(MainActivity.this, String.format("Progress %d/%d", bytesLoaded, bytesTotal), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
