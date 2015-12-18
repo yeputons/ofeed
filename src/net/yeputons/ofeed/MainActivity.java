f4d21696f34117d4d5a4782ae89d9c80f02a5455
Egor Suvorov <egor.suvorov@gmail.com>
2015-12-18 03:28:06 +0300

    Clear deep saved web pages on clearCache

--- src/net/yeputons/ofeed/MainActivity.java
+++ src/net/yeputons/ofeed/MainActivity.java
@@ -147,6 +147,7 @@ public class MainActivity extends Activity implements VKCallback<VKAccessToken> 
             h.getCachedGroupDao().deleteBuilder().delete();
             h.getCachedUserDao().deleteBuilder().delete();
             h.getCachedWebResourcesDao().deleteBuilder().delete();
+            h.getCachedWebPageDao().deleteBuilder().delete();
         } catch (SQLException e) {
             Log.e(TAG, "Unable to clear cache", e);
             Toast.makeText(this, "Error while clearing cache, it may become inconsistent", Toast.LENGTH_LONG).show();
