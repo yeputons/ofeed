b3270857ee91e44bfc395f3e8a938859747ec6ac
Egor Suvorov <egor.suvorov@gmail.com>
2016-03-07 13:57:17 +0300

    OfeedApplication: structure ResourceDownloader creation better

--- src/net/yeputons/ofeed/OfeedApplication.java
+++ src/net/yeputons/ofeed/OfeedApplication.java
@@ -1,23 +1,34 @@
 package net.yeputons.ofeed;
 
 import android.app.Application;
+import android.app.DownloadManager;
+import android.content.Context;
 import android.support.annotation.NonNull;
 import com.vk.sdk.VKSdk;
 import net.yeputons.ofeed.db.DbHelper;
 import net.yeputons.ofeed.web.ResourceDownloader;
+import net.yeputons.ofeed.web.dmanager.DownloadManagerResourceDownloader;
 import net.yeputons.ofeed.web.dmanager.ThreadPoolResourceDownloader;
 
 public class OfeedApplication extends Application {
-    //private static DownloadManagerResourceDownloader downloader;
-    private static ThreadPoolResourceDownloader downloader;
+    private static ResourceDownloader downloader;
 
     @Override
     public void onCreate() {
         super.onCreate();
         VKSdk.initialize(getApplicationContext());
         DbHelper.initializeHelper(getApplicationContext());
-        //downloader = new DownloadManagerResourceDownloader(
-        //        (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE));
+        createThreadPoolResourceDownloader();
+    }
+
+    // Use either of the following two methods to specify application-wide downloader
+
+    private void createDownloadManagerResourceDownloader() {
+        downloader = new DownloadManagerResourceDownloader(
+                (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE));
+    }
+
+    private void createThreadPoolResourceDownloader() {
         downloader = new ThreadPoolResourceDownloader();
     }
 
